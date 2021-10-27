f93b16e1626d7ad4b39735f5422835c3d9b61748
ShubhamPV <shubhamvadnere5@gmail.com>
2021-10-27 12:58:02 +0530

    [ShubhamPV] CheckFile

---
+++ FileIOAB/FileIO/FileReadWriteOperation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIOAB.FileIO
+{
+    class FileReadWriteOperation
+    {
+        public bool FilExists(string path)
+        {
+            if (File.Exists(path))
+            {
+                Console.WriteLine("File exists");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("File not exists");
+                return false;
+            }
+        }
+
+
+    }
+}
---
+++ FileIOAB/Program.cs
@@ -0,0 +1,21 @@
+﻿using FileIOAB.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIOAB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to File IO Operations");
+            string path = @"C:\Users\OmSaiRam\Downloads\Practicals\FileIOAB\FileIOAB\FileIO\Sample.txt";
+            FileReadWriteOperation file = new FileReadWriteOperation();
+            file.FilExists(path);
+            Console.ReadLine();
+        }
+    }
+}
